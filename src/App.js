6e06ee4fa59db8d995faa220cde172c7d735ee75
Yaisa Arteaga <yaisamichellea@gmail.com>
2022-10-07 11:42:00 -0500

    style details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import "./App.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About/>}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </BrowserRouter>
   );
